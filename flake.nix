7350c3ab618bd7259e4f72dd136dea524cb198e3
Scott Trinh <scott@ca.la>
2022-11-30 20:50:47 -0500

    Basic Nix flake and empty package.json

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "minimal repro for surreal.wasm issue";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                nodejs-18_x
+              ];
+          };
+        });
+}
